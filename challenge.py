11d9bc3ad448a13b32e87253402b885a30138ea4
Xavier Martinez <xaviermartinez2012@gmail.com>
2018-09-15 01:23:48 -0700

    initial commit

---
+++ challenge.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import sys
+
+print(f'python{sys.version}')
+print(f'pandas --version {pd.__version__}')
+print(f'numpy --version {np.__version__}')
+print(f'scikit-learn --version {sklearn.__version__}')
